0cce92271d434928bb4a32b8ce1d7f398fefc610
ernestobone98 <ernestobone98>
2023-11-21 12:01:36 +0100

    cardealer first step

--- data_treater/cardealer_treater.py
+++ data_treater/cardealer_treater.py
@@ -1,14 +1,32 @@
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
-def treat_cardealer(sc: SparkContext):
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_replace
 
-    cardealer = sc.textFile("/user/hduser/cardealer.csv")
-    # Split each line of the csv files by the delimiter ";"
-    cardealer = cardealer.map(lambda line: line.split(";"))
+def treat_cardealer(spark: SparkSession):
+    # Read CSV into DataFrame
+    cardealer = spark.read.option("delimiter", ";").csv("/user/hduser/cardealer.csv", header=True)
 
-    # Remove the header of each csv file
-    cardealer_header = cardealer.first()
-    cardealer = cardealer.filter(lambda line: line != cardealer_header)
+    # Drop rows with null values
+    cardealer = cardealer.na.drop()
 
-    # Create a RDD for each csv file
-    cardealerRDD = cardealer.map(lambda line: (line[0], line[1], line[2], line[3], line[4]))
+    # replace "Hyunda*" (RE) by "Hyundai"
+    cardealer = cardealer.withColumn("marque", regexp_replace("marque", r"Hyunda\*", "Hyundai"))
+
+    # In the column "longueur" replace "courte" by "1", "moyenne" by "2", "longue" by "3" and "tr\*s longue" by "4"
+    cardealer = cardealer.withColumn("longueur", regexp_replace("longueur", r"courte", "1"))
+    cardealer = cardealer.withColumn("longueur", regexp_replace("longueur", r"moyenne", "2"))
+    cardealer = cardealer.withColumn("longueur", regexp_replace("longueur", r"longue", "3"))
+    cardealer = cardealer.withColumn("longueur", regexp_replace("longueur", r"tr\*s longue", "4"))
+
+    # Convert the column "longueur", "nbPlaces", "nbPortes" and "puissance" to integer
+    cardealer = cardealer.withColumn("longueur", cardealer["longueur"].cast("int"))
+    cardealer = cardealer.withColumn("nbPlaces", cardealer["nbPlaces"].cast("int"))
+    cardealer = cardealer.withColumn("nbPortes", cardealer["nbPortes"].cast("int"))
+    cardealer = cardealer.withColumn("puissance", cardealer["puissance"].cast("int"))
+
+    # Convert the column "prix" to float
+    cardealer = cardealer.withColumn("prix", cardealer["prix"].cast("float"))
+
+    # To show the resulting DataFrame
+    cardealer.show()
