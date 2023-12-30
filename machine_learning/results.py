f12349980be53956a747266fe71b1807533f9f3f
Mike Chiappe <mike.chiappe2@gmail.com>
2023-12-30 18:23:17 +0100

    add commit of results for marketing customer best car prediction

--- machine_learning/results.py
+++ machine_learning/results.py
@@ -1,5 +1,16 @@
 import joblib
 import marketing_treater as mt
+from databus import Databus
+import yaml
+import asyncio
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+databus = Databus(config["nats"])
+databus_connect_task = databus.connect()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(databus_connect_task)
 
 # Get the "customer_marketing" file
 print("---------- Getting the data ----------")
@@ -27,3 +38,14 @@ print(marketing.head())
 # Save marketing DataFrame to a CSV file called "predictions.csv"
 print("---------- Saving Predictions ----------")
 marketing.to_csv("predictions.csv", index=False)
+
+# publish_results()
+def transform_data(row_tuple):
+    _, row = row_tuple
+    return {
+        "customer_marketing_id": row["id"],
+        "catalog_car_id": row["prediction"]
+    }
+
+databus_publish_task = databus.publish_result("customer_markting_analysis_data", marketing.iterrows(), "customer_marketing_id", transform_data, mode="upsert")
+loop.run_until_complete(databus_publish_task)
